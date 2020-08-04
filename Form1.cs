1eff2025a44f67107b1d112f4b3f67da49ac19e6
AnkitaVuppala <41575325+AnkitaVuppala@users.noreply.github.com>
2020-08-04 18:13:47 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Banktransaction
+{
+    public partial class Form1 : Form
+    {
+        string minbal;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=ANKITA;Initial Catalog=bank;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd1 = new SqlCommand("SELECT Balance FROM    Data_details WHERE  Balance= (SELECT MIN(Balance)  FROM Data_details); ", con);
+            minbal = (string)cmd1.ExecuteScalar();
+            int bal = int.Parse(minbal);
+            bal = bal - int.Parse(textBox3.Text);
+            string sql = "insert into Data_details(Acc_id,Acc_name,Amount,Balance) values('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + bal + "')";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            int i = cmd.ExecuteNonQuery();
+            if (i != 0)
+            {
+                MessageBox.Show("Transferred Successfully....Avl bal: " + bal);
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+            cmd.Dispose();
+            con.Close();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 s = new Form2();
+            s.Show();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Form2.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Banktransaction
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bankDataSet = new Banktransaction.bankDataSet();
+            this.datadetailsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.data_detailsTableAdapter = new Banktransaction.bankDataSetTableAdapters.Data_detailsTableAdapter();
+            this.balanceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.amountDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.accnameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.accidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bankDataSet1 = new Banktransaction.bankDataSet();
+            ((System.ComponentModel.ISupportInitialize)(this.bankDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.datadetailsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bankDataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bankDataSet
+            // 
+            this.bankDataSet.DataSetName = "bankDataSet";
+            this.bankDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // datadetailsBindingSource
+            // 
+            this.datadetailsBindingSource.DataMember = "Data_details";
+            this.datadetailsBindingSource.DataSource = this.bankDataSet;
+            // 
+            // data_detailsTableAdapter
+            // 
+            this.data_detailsTableAdapter.ClearBeforeFill = true;
+            // 
+            // balanceDataGridViewTextBoxColumn
+            // 
+            this.balanceDataGridViewTextBoxColumn.DataPropertyName = "Balance";
+            this.balanceDataGridViewTextBoxColumn.HeaderText = "Balance";
+            this.balanceDataGridViewTextBoxColumn.Name = "balanceDataGridViewTextBoxColumn";
+            // 
+            // amountDataGridViewTextBoxColumn
+            // 
+            this.amountDataGridViewTextBoxColumn.DataPropertyName = "Amount";
+            this.amountDataGridViewTextBoxColumn.HeaderText = "Amount";
+            this.amountDataGridViewTextBoxColumn.Name = "amountDataGridViewTextBoxColumn";
+            // 
+            // accnameDataGridViewTextBoxColumn
+            // 
+            this.accnameDataGridViewTextBoxColumn.DataPropertyName = "Acc_name";
+            this.accnameDataGridViewTextBoxColumn.HeaderText = "Acc_name";
+            this.accnameDataGridViewTextBoxColumn.Name = "accnameDataGridViewTextBoxColumn";
+            // 
+            // accidDataGridViewTextBoxColumn
+            // 
+            this.accidDataGridViewTextBoxColumn.DataPropertyName = "Acc_id";
+            this.accidDataGridViewTextBoxColumn.HeaderText = "Acc_id";
+            this.accidDataGridViewTextBoxColumn.Name = "accidDataGridViewTextBoxColumn";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.accidDataGridViewTextBoxColumn,
+            this.accnameDataGridViewTextBoxColumn,
+            this.amountDataGridViewTextBoxColumn,
+            this.balanceDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.datadetailsBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(206, 105);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(406, 206);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // bankDataSet1
+            // 
+            this.bankDataSet1.DataSetName = "bankDataSet";
+            this.bankDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bankDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.datadetailsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bankDataSet1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private bankDataSet bankDataSet;
+        private System.Windows.Forms.BindingSource datadetailsBindingSource;
+        private bankDataSetTableAdapters.Data_detailsTableAdapter data_detailsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn balanceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn amountDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn accnameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn accidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private bankDataSet bankDataSet1;
+    }
+}
